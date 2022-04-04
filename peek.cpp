edb0a4e530a8f2cf2dd9020eed43161e771f1d89
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2022-04-03 21:53:36 -0400

    some fun

--- peek.cpp
+++ peek.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
-int findPeek(std::vector<int>& arr) {
-    
-}
 
 int main() {
+    typedef std::priority_queue<int, std::vector<int>, std::greater<int>> MinPQ;
+    MinPQ pq{};
+    pq.push(43);
+    pq.push(23);
+    std::cout << pq.top() << std::endl;
     return 0;
 }
